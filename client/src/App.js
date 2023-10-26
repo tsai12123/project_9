2dbd0ef6168feb77c11a190caa99d9f6bd43de68
bo chen <bochen@gmail.com>
2023-10-26 20:40:04 +0800

    debug

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ function App() {
           }
         >
           <Route index element={<HomeComponent />} />
-          <Route path="register" element={<RegisterComponent />} />
+          <Route path="/register" element={<RegisterComponent />} />
           <Route
             path="login"
             element={
