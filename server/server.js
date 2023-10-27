d6011741bef27fa1e48a066b80586be2cfe93cf1
bo chen <bochen@gmail.com>
2023-10-27 21:04:56 +0800

    debug

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ if (
   process.env.NODE_ENV === "staging"
 ) {
   app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "public ", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "public", "index.html"));
   });
 }
 
